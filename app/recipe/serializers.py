28cb854d4b2eef371ec13a5e37256559ec94ea90
anik107 <purplecs107@yahoo.com>
2025-04-22 20:16:01 +0600

    [MOD] Tag Api modified

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,16 +26,32 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id']
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create a recipe with tags."""
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context['request'].user
-
-        for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
-            recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe with tags."""
+        tags = validated_data.pop('tags', [])
+        if tags is not None:
+            # Clear existing tags
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
